d4a0d03f3e68ec48d1106171f756efdc6bd5dba2
Mahmoud Khodrawi <mahmoud.k2@gso.schule.koeln>
2022-03-10 11:58:31 +0100

    Aufgabe Fertig

--- Array_Noten/Program.cs
+++ Array_Noten/Program.cs
@@ -6,8 +6,30 @@ namespace Array_Noten
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //Test
+            int n = 20;
+            double summe = 0;
+
+            int[] Noten = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Gebben sie die Note {i} ein : ");
+                Noten[i] = Convert.ToInt32(Console.ReadLine());
+                summe = summe + Noten[i];
+            }
+            
+            Console.WriteLine("Das sind ihre B");
+            for (int p = 0; p < Noten.Length; p++)
+            {
+                Console.WriteLine(Noten[p]);
+            }
+
+            Console.WriteLine("Der Durschnittt ist");
+            Console.WriteLine(summe/n);
+
+
+
+
+
         }
     }
 }
