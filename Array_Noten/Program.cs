201c67c610373b03b6c3eb4912a5fa9d1e8dfcff
Mahmoud Khodrawi <mahmoud.k2@gso.schule.koeln>
2022-03-10 09:55:02 +0100

    Projektdateien hinzufügen.

---
+++ Array_Noten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Array_Noten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
